6cb8a433c1831c82288266f775035539c2c3787c
Dani Sarabia <dlsm@gft.com>
2022-06-16 14:46:04 +0200

    Add plugin PMD to build.gradle

--- src/main/java/com/example/springtesting/HomeController.java
+++ src/main/java/com/example/springtesting/HomeController.java
@@ -1,6 +1,6 @@
 package com.example.springtesting;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController

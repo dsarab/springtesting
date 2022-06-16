ca7e27ee86e1bad88bf8e806a56450eeec7f4be9
Dani Sarabia <dlsm@gft.com>
2022-06-16 14:10:50 +0200

    Add plugin PMD to build.gradle

--- src/main/java/com/example/springtesting/HomeController.java
+++ src/main/java/com/example/springtesting/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController {
           @RequestParam(value = "b", defaultValue = "0") Float b
     ) {
         if(a==0){
-            return a;
+            return b;
         }
         return a+b;
     }

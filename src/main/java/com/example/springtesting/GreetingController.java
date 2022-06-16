e8f43b0ddb46b309a6a7853d66da9abfd262b9f9
Dani Sarabia <dlsm@gft.com>
2022-06-16 15:53:04 +0200

    Add check to launch pmd plugin

--- src/main/java/com/example/springtesting/GreetingController.java
+++ src/main/java/com/example/springtesting/GreetingController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
 @Controller
 public class GreetingController {
 
-    private final GreetingService service;
+    final transient GreetingService service;
 
     public GreetingController(GreetingService service) {
         this.service = service;
